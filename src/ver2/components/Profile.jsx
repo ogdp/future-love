bb1ed88e579f994a837063337284dea6ee383540
MinhDuc <ogdp.vn@gmail.com>
2023-08-08 15:14:58 +0700

    Fix css

--- src/ver2/components/Profile.jsx
+++ src/ver2/components/Profile.jsx
@@ -425,15 +425,30 @@ export default function () {
                     <p>Comments</p>
                   </div>
                 </div>
-                <div className="flex justify-center items-center py-4 gap-3">
-                  <button className=" bg-white shadow-gray-500 rounded-full py-2 px-5">
+                <div className="flex justify-center items-center py-4 gap-3 md:my-8">
+                  <button className=" bg-white shadow-gray-500 rounded-full py-2 px-5 text-[14px]">
                     View Events
                   </button>
                   <button
                     onClick={() => openModal()}
-                    className="md:hidden bg-white shadow-gray-500 rounded-full py-2 px-5"
+                    className="md:hidden bg-white shadow-gray-500 rounded-full py-2 px-5 text-[14px]"
                   >
-                    Edit your profile
+                    <div className="flex justify-center items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        style={{
+                          fill: "rgba(0, 0, 0, 1)",
+                          transform: "",
+                          msfilter: "",
+                        }}
+                      >
+                        <path d="M11.587 6.999H7.702a2 2 0 0 0-1.88 1.316l-3.76 10.342c-.133.365-.042.774.232 1.049l.293.293 6.422-6.422c-.001-.026-.008-.052-.008-.078a1.5 1.5 0 1 1 1.5 1.5c-.026 0-.052-.007-.078-.008l-6.422 6.422.293.293a.997.997 0 0 0 1.049.232l10.342-3.761a2 2 0 0 0 1.316-1.88v-3.885L19 10.414 13.586 5l-1.999 1.999zm8.353 2.062-5-5 2.12-2.121 5 5z" />
+                      </svg>
+                      <span> Edit </span>
+                    </div>
                   </button>
                 </div>
               </div>
@@ -442,9 +457,24 @@ export default function () {
           <div className="">
             <button
               onClick={() => openModal()}
-              className="max-lg:hidden lg:mt-20 max-lg:fixed ml-48 -mt-60 my-2 bg-white shadow-gray-500 rounded-full md:w-[150px] md:h-[25px]"
+              className="max-lg:hidden bg-white shadow-gray-500 rounded-full py-2 px-5 text-[14px] my-8 mx-7"
             >
-              Edit your profile
+              <div className="flex justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  style={{
+                    fill: "rgba(0, 0, 0, 1)",
+                    transform: "",
+                    msfilter: "",
+                  }}
+                >
+                  <path d="M11.587 6.999H7.702a2 2 0 0 0-1.88 1.316l-3.76 10.342c-.133.365-.042.774.232 1.049l.293.293 6.422-6.422c-.001-.026-.008-.052-.008-.078a1.5 1.5 0 1 1 1.5 1.5c-.026 0-.052-.007-.078-.008l-6.422 6.422.293.293a.997.997 0 0 0 1.049.232l10.342-3.761a2 2 0 0 0 1.316-1.88v-3.885L19 10.414 13.586 5l-1.999 1.999zm8.353 2.062-5-5 2.12-2.121 5 5z" />{" "}
+                </svg>
+                <span> Edit your profile</span>
+              </div>
             </button>
             {showModal ? (
               <>
